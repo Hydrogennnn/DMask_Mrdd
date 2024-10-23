355bff3fa6eda8dc8a879570f5656ab2eaaa500b
Jiedong <2902704036@qq.com>
2024-10-23 21:37:22 +0800

    add random seed

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -97,6 +97,7 @@ def reproducibility_setting(seed):
     """
     np.random.seed(seed)
     torch.manual_seed(seed)
+    random.seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.backends.cudnn.deterministic = True
